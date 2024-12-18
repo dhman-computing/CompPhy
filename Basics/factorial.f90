f24184a8aad760275e518e924d7d924045020011
Dhiman Kumar Chakrabarty <78009274+dhman-computing@users.noreply.github.com>
2024-12-18 12:25:33 +0530

    editted factorial.f90 to include both the "do" and "do while" loops

--- Basics/factorial.f90
+++ Basics/factorial.f90
@@ -5,11 +5,20 @@ program factorial
   integer(kind=16) :: res
 
   res = 1
-
   do i = 1, 30, 1
 
     res = res*i
     print "(a13, i2, a6, i39)", "Factorial of ", i, " is : ",  res
+
+  end do
+
+  i = 1
+  res = 1
+  do while (i <= 30)
+
+    res = res*i
+    print "(a13, i2, a6, i39)", "Factorial of ", i, " is : ",  res
+    i = i + 1
 
   end do
 
